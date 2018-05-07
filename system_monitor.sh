87a865bbeab2d728cb6a70864009fb090e74b9de
Boopathi Duraisamy <boopathid@users.noreply.github.com>
2018-05-07 11:59:44 -0700

    Update system_monitor.sh
    #Linux Monitoring script:
    # Outputs Logged in Users, Disk usage, Load, Uptime, IP info, OS etc.
    # Copy the script, chmod 755 system-monitor.sh
    # ./system-monitor.sh -i   This will install the script
    # monitor This command will output the information needed.

--- system_monitor.sh
+++ system_monitor.sh
@@ -52,6 +52,7 @@ ping -c 1 google.com &> /dev/null && echo -e '\E[32m'"Internet: $tecreset Connec
 os=$(uname -o)
 echo -e '\E[32m'"Operating System Type :" $tecreset $os
 
+
 # Check OS Release Version and Name
 cat /etc/os-release | grep 'NAME\|VERSION' | grep -v 'VERSION_ID' | grep -v 'PRETTY_NAME' > /tmp/osrelease
 echo -n -e '\E[32m'"OS Name :" $tecreset  && cat /tmp/osrelease | grep -v "VERSION" | cut -f2 -d\"
